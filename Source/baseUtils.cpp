1cd14ceffb3de7bd910b51bf6406131872b8838d
Adam Sawicki <sawickiap@poczta.onet.pl>
2022-01-06 04:22:27 +0100

    4

--- Source/baseUtils.cpp
+++ Source/baseUtils.cpp
@@ -21,13 +21,9 @@ Define this macro to enable C++17 compatibility.
 #include <cstdint>
 #include <cstdarg>
 
-inline size_t tstrlen(const char* sz) { return strlen(sz); }
 inline size_t tstrlen(const wchar_t* sz) { return wcslen(sz); }
-inline void tstrcpy(char* dst, size_t dstCapacity, const char* src) { strcpy_s(dst, dstCapacity, src); }
 inline void tstrcpy(wchar_t* dst, size_t dstCapacity, const wchar_t* src) { wcscpy_s(dst, dstCapacity, src); }
-inline int tstrncmp(const char* lhs, const char* rhs, size_t count) { return strncmp(lhs, rhs, count); }
 inline int tstrncmp(const wchar_t* lhs, const wchar_t* rhs, size_t count) { return wcsncmp(lhs, rhs, count); }
-inline int tstrnicmp(const char* lhs, const char* rhs, size_t count) { return _strnicmp(lhs, rhs, count); }
 inline int tstrnicmp(const wchar_t* lhs, const wchar_t* rhs, size_t count) { return _wcsnicmp(lhs, rhs, count); }
 
 template<typename CharT>
@@ -63,12 +59,7 @@ public:
 
     inline str_view_template<CharT>& operator=(str_view_template<CharT>&& src);
 
-    inline void swap(str_view_template<CharT>& rhs) noexcept;
-
-
     inline size_t length() const;
-
-    inline size_t size() const { return length(); }
 
     inline bool empty() const;
 
@@ -78,40 +69,9 @@ public:
 
     inline const CharT* end() const { return m_Begin + length(); }
 
-    inline const CharT front() const { return *m_Begin; }
-
-    inline const CharT back() const { return m_Begin[length() - 1]; }
-    
     inline CharT operator[](size_t index) const { return m_Begin[index]; }
-    inline CharT at(size_t index) const { return m_Begin[index]; }
 
     inline const CharT* c_str() const;
-
-    inline str_view_template<CharT> substr(size_t offset = 0, size_t length = SIZE_MAX) const;
-
-    inline size_t copy_to(CharT* dst, size_t offset = 0, size_t length = SIZE_MAX) const;
-
-    inline void to_string(StringT& dst, size_t offset = 0, size_t length = SIZE_MAX) const;
-
-#if STR_VIEW_CPP17
-    inline void to_string_view(StringViewT& dst, size_t offset = 0, size_t length = SIZE_MAX) const;
-#endif
-
-    inline int compare(const str_view_template<CharT>& rhs, bool case_sensitive = true) const;
-
-    inline bool operator==(const str_view_template<CharT>& rhs) const { return compare(rhs) == 0; }
-    inline bool operator!=(const str_view_template<CharT>& rhs) const { return compare(rhs) != 0; }
-    inline bool operator< (const str_view_template<CharT>& rhs) const { return compare(rhs) <  0; }
-    inline bool operator> (const str_view_template<CharT>& rhs) const { return compare(rhs) >  0; }
-    inline bool operator<=(const str_view_template<CharT>& rhs) const { return compare(rhs) <= 0; }
-    inline bool operator>=(const str_view_template<CharT>& rhs) const { return compare(rhs) >= 0; }
-
-	inline bool starts_with(CharT prefix, bool case_sensitive = true) const;
-    inline bool starts_with(const str_view_template<CharT>& prefix, bool case_sensitive = true) const;
-
-    inline bool ends_with(CharT suffix, bool case_sensitive = true) const;
-    inline bool ends_with(const str_view_template<CharT>& suffix, bool case_sensitive = true) const;
-
 
 private:
     mutable size_t m_Length;
@@ -283,14 +243,6 @@ inline str_view_template<CharT>& str_view_template<CharT>::operator=(str_view_te
 }
 
 template<typename CharT>
-inline void str_view_template<CharT>::swap(str_view_template<CharT>& rhs) noexcept
-{
-    std::swap(m_Length, rhs.m_Length);
-    std::swap(m_Begin, rhs.m_Begin);
-    std::swap(m_NullTerminatedPtr, rhs.m_NullTerminatedPtr);
-}
-
-template<typename CharT>
 inline size_t str_view_template<CharT>::length() const
 {
     if(m_Length == SIZE_MAX)
@@ -335,135 +287,6 @@ inline const CharT* str_view_template<CharT>::c_str() const
         m_NullTerminatedPtr = nullTerminated;
     }
 	return m_NullTerminatedPtr;
-}
-
-template<typename CharT>
-inline size_t str_view_template<CharT>::copy_to(CharT* dst, size_t offset, size_t length) const
-{
-    const size_t thisLen = this->length();
-    assert(offset <= thisLen);
-    length = std::min(length, thisLen - offset);
-    memcpy(dst, m_Begin + offset, length * sizeof(CharT));
-    return length;
-}
-
-template<typename CharT>
-inline void str_view_template<CharT>::to_string(StringT& dst, size_t offset, size_t length) const
-{
-    const size_t thisLen = this->length();
-    assert(offset <= thisLen);
-    length = std::min(length, thisLen - offset);
-    dst.assign(m_Begin + offset, m_Begin + (offset + length));
-}
-
-#if STR_VIEW_CPP17
-
-template<typename CharT>
-inline void str_view_template<CharT>::to_string_view(StringViewT& dst, size_t offset, size_t length) const
-{
-    const size_t thisLen = this->length();
-    assert(offset <= thisLen);
-    length = std::min(length, thisLen - offset);
-    dst = StringViewT(m_Begin + offset, length);
-}
-
-#endif // #if STR_VIEW_CPP17
-
-template<typename CharT>
-inline str_view_template<CharT> str_view_template<CharT>::substr(size_t offset, size_t length) const
-{
-    // Length can remain unknown.
-    if(m_Length == SIZE_MAX && length == SIZE_MAX)
-    {
-        assert(m_NullTerminatedPtr == m_Begin);
-        return str_view_template<CharT>(m_Begin + offset);
-    }
-
-    const size_t thisLen = this->length();
-    assert(offset <= thisLen);
-	length = std::min(length, thisLen - offset);
-	// Result will be null-terminated.
-	if(m_NullTerminatedPtr == m_Begin && length == thisLen - offset)
-		return str_view_template<CharT>(m_Begin + offset, length, StillNullTerminated());
-	// Result will not be null-terminated.
-	return str_view_template<CharT>(m_Begin + offset, length);
-}
-
-template<typename CharT>
-inline int str_view_template<CharT>::compare(const str_view_template<CharT>& rhs, bool case_sensitive) const
-{
-    const size_t lhsLen = length();
-    const size_t rhsLen = rhs.length();
-    const size_t minLen = std::min(lhsLen, rhsLen);
-
-    if(minLen > 0)
-    {
-        const int result = case_sensitive ?
-            tstrncmp(data(), rhs.data(), minLen) :
-            tstrnicmp(data(), rhs.data(), minLen);
-        if(result != 0)
-            return result;
-    }
-
-    if(lhsLen < rhsLen)
-        return -1;
-    if(lhsLen > rhsLen)
-        return 1;
-    return 0;
-}
-
-template<typename CharT>
-inline bool str_view_template<CharT>::starts_with(CharT prefix, bool case_sensitive) const
-{
-    if(!empty())
-    {
-        if(case_sensitive)
-            return *m_Begin == prefix;
-        return tstrnicmp(m_Begin, &prefix, 1) == 0;
-    }
-    return false;
-}
-
-template<typename CharT>
-inline bool str_view_template<CharT>::starts_with(const str_view_template<CharT>& prefix, bool case_sensitive) const
-{
-    const size_t prefixLen = prefix.length();
-    if(length() >= prefixLen)
-    {
-        const int cmpResult = case_sensitive ?
-            tstrncmp(m_Begin, prefix.m_Begin, prefixLen) :
-            tstrnicmp(m_Begin, prefix.m_Begin, prefixLen);
-        return cmpResult == 0;
-    }
-    return false;
-}
-
-template<typename CharT>
-inline bool str_view_template<CharT>::ends_with(CharT suffix, bool case_sensitive) const
-{
-    const size_t thisLen = length();
-    if(thisLen > 0)
-    {
-        if(case_sensitive)
-            return m_Begin[thisLen - 1] == suffix;
-        return tstrnicmp(m_Begin + (thisLen - 1), &suffix, 1) == 0;
-    }
-    return false;
-}
-
-template<typename CharT>
-inline bool str_view_template<CharT>::ends_with(const str_view_template<CharT>& suffix, bool case_sensitive) const
-{
-    const size_t thisLen = length();
-    const size_t suffixLen = suffix.length();
-    if(thisLen >= suffixLen)
-    {
-        const int cmpResult = case_sensitive ?
-            tstrncmp(m_Begin + (thisLen - suffixLen), suffix.m_Begin, suffixLen) :
-            tstrnicmp(m_Begin + (thisLen - suffixLen), suffix.m_Begin, suffixLen);
-        return cmpResult == 0;
-    }
-    return false;
 }
 
 void Format(const wstr_view& format, ...)
