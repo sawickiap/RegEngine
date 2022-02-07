b0777fa6dd286295c38992b623cb27e4c84c0c77
Adam Sawicki <sawickiap@poczta.onet.pl>
2022-02-07 20:55:28 +0100

    class Texture: Added mipmap generation

--- Source/Texture.cpp
+++ Source/Texture.cpp
@@ -10,7 +10,7 @@ Texture::~Texture()
     g_Renderer->GetSRVDescriptorManager()->FreePersistent(m_Descriptor);
 }
 
-void Texture::LoadFromFile(const wstr_view& filePath)
+void Texture::LoadFromFile(uint32_t flags, const wstr_view& filePath)
 {
     assert(IsEmpty());
 
@@ -48,31 +48,71 @@ void Texture::LoadFromFile(const wstr_view& filePath)
     CHECK_BOOL(img->height == metadata.height);
     CHECK_BOOL(img->format == metadata.format);
 
+    if((flags & FLAG_SRGB) != 0)
+    {
+        const DXGI_FORMAT SRGBFormat = DirectX::MakeSRGB(img->format);
+        CHECK_BOOL(SRGBFormat != DXGI_FORMAT_UNKNOWN);
+        scratchImage.OverrideFormat(SRGBFormat);
+    }
+
     LogInfoF(L"  Width={}, Height={}, Format={}",
         metadata.width, metadata.height, (uint32_t)metadata.format);
 
-    D3D12_RESOURCE_DESC resDesc = CD3DX12_RESOURCE_DESC::Tex2D(
-        DirectX::MakeSRGB(metadata.format),
-        (uint64_t)metadata.width,
-        (uint32_t)metadata.height,
-        (uint16_t)metadata.arraySize,
-        (uint16_t)metadata.mipLevels,
-        1, // sampleCount
-        0, // sampleQuality
-        D3D12_RESOURCE_FLAG_NONE);
+    if((flags & FLAG_GENERATE_MIPMAPS) != 0)
+    {
+        LogInfo(L"  Generating mipmaps...");
 
-    D3D12_SUBRESOURCE_DATA subresourceData = {
-        .pData = img->pixels,
-        .RowPitch = (LONG_PTR)img->rowPitch,
-        .SlicePitch = (LONG_PTR)img->slicePitch
-    };
-
-    LoadFromMemory(resDesc, subresourceData, filePath);
+        DirectX::ScratchImage mipScratchImage;
+        CHECK_HR(DirectX::GenerateMipMaps(*img, DirectX::TEX_FILTER_DEFAULT, 0, mipScratchImage));
+        const DirectX::TexMetadata& mipMetadata = mipScratchImage.GetMetadata();
+        m_Desc = CD3DX12_RESOURCE_DESC::Tex2D(
+            mipMetadata.format,
+            (uint64_t)mipMetadata.width,
+            (uint32_t)mipMetadata.height,
+            (uint16_t)mipMetadata.arraySize,
+            (uint16_t)mipMetadata.mipLevels,
+            1, // sampleCount
+            0, // sampleQuality
+            D3D12_RESOURCE_FLAG_NONE);
+            CreateTexture(filePath);
+        CHECK_BOOL(mipScratchImage.GetImageCount() == mipMetadata.mipLevels);
+        for(uint32_t mip = 0; mip < (uint32_t)mipMetadata.mipLevels; ++mip)
+        {
+            const DirectX::Image* const mipImg = mipScratchImage.GetImage(mip, 0, 0);
+            D3D12_SUBRESOURCE_DATA subresourceData = {
+                .pData = mipImg->pixels,
+                .RowPitch = (LONG_PTR)mipImg->rowPitch,
+                .SlicePitch = (LONG_PTR)mipImg->slicePitch
+            };
+            const bool lastLevel = mip == mipMetadata.mipLevels - 1;
+            UploadMipLevel(mip, uvec2((uint32_t)mipImg->width, (uint32_t)mipImg->height), subresourceData, lastLevel);
+        }
+        CreateDescriptor();
+    }
+    else
+    {
+        D3D12_RESOURCE_DESC resDesc = CD3DX12_RESOURCE_DESC::Tex2D(
+            metadata.format,
+            (uint64_t)metadata.width,
+            (uint32_t)metadata.height,
+            (uint16_t)metadata.arraySize,
+            (uint16_t)metadata.mipLevels,
+            1, // sampleCount
+            0, // sampleQuality
+            D3D12_RESOURCE_FLAG_NONE);
+        D3D12_SUBRESOURCE_DATA subresourceData = {
+            .pData = img->pixels,
+            .RowPitch = (LONG_PTR)img->rowPitch,
+            .SlicePitch = (LONG_PTR)img->slicePitch
+        };
+        LoadFromMemory(0, resDesc, subresourceData, filePath);
+    }
 
     ERR_CATCH_MSG(std::format(L"Cannot load texture from \"{}\".", filePath));
 }
 
 void Texture::LoadFromMemory(
+    uint32_t flags,
     const D3D12_RESOURCE_DESC& resDesc,
     const D3D12_SUBRESOURCE_DATA& data,
     const wstr_view& name)
@@ -80,14 +120,43 @@ void Texture::LoadFromMemory(
     assert(IsEmpty());
 
     ERR_TRY;
+    
+    m_Desc = resDesc;
+    CreateTexture(name);
+    UploadMipLevel(0, uvec2((uint32_t)resDesc.Width, resDesc.Height), data,
+        true); // lastLevel
+    CreateDescriptor();
 
-    const uint8_t bitsPerPixel = DXGIFormatToBitsPerPixel(resDesc.Format);
+    ERR_CATCH_FUNC;
+}
+
+void Texture::CreateTexture(const wstr_view& name)
+{
+    assert(m_Desc.Format != DXGI_FORMAT_UNKNOWN);
+    D3D12MA::ALLOCATION_DESC allocDesc = {};
+    allocDesc.HeapType = D3D12_HEAP_TYPE_DEFAULT;
+    CHECK_HR(g_Renderer->GetMemoryAllocator()->CreateResource(
+        &allocDesc,
+        &m_Desc,
+        D3D12_RESOURCE_STATE_COPY_DEST,
+        nullptr, // pOptimizedClearValue
+        &m_Allocation,
+        IID_PPV_ARGS(&m_Resource))); // riidResource, ppvResource
+    if(!name.empty())
+        SetD3D12ObjectName(m_Resource.Get(), name);
+}
+
+void Texture::UploadMipLevel(uint32_t mipLevel, const uvec2& size, const D3D12_SUBRESOURCE_DATA& data,
+    bool lastLevel)
+{
+    assert(m_Desc.Format != DXGI_FORMAT_UNKNOWN);
+    const uint8_t bitsPerPixel = DXGIFormatToBitsPerPixel(m_Desc.Format);
     CHECK_BOOL(bitsPerPixel > 0 && bitsPerPixel % 8 == 0);
     const uint32_t bytesPerPixel = bitsPerPixel / 8;
 
     // Create source buffer.
-    const size_t srcBufRowPitch = std::max<size_t>(resDesc.Width * bytesPerPixel, D3D12_TEXTURE_DATA_PITCH_ALIGNMENT);
-    const size_t srcBufSize = srcBufRowPitch * resDesc.Height;
+    const size_t srcBufRowPitch = std::max<size_t>(size.x * bytesPerPixel, D3D12_TEXTURE_DATA_PITCH_ALIGNMENT);
+    const size_t srcBufSize = srcBufRowPitch * size.y;
     ComPtr<D3D12MA::Allocation> srcBuf;
     {
         const CD3DX12_RESOURCE_DESC desc = CD3DX12_RESOURCE_DESC::Buffer(srcBufSize);
@@ -109,28 +178,13 @@ void Texture::LoadFromMemory(
         char* srcBufMappedPtr = nullptr;
         const char* textureDataPtr = (const char*)data.pData;
         CHECK_HR(srcBuf->GetResource()->Map(0, D3D12_RANGE_NONE, (void**)&srcBufMappedPtr));
-        for(uint32_t y = 0; y < resDesc.Height; ++y)
+        for(uint32_t y = 0; y < size.y; ++y)
         {
-            memcpy(srcBufMappedPtr, textureDataPtr, resDesc.Width * bytesPerPixel);
+            memcpy(srcBufMappedPtr, textureDataPtr, size.x * bytesPerPixel);
             textureDataPtr = (char*)textureDataPtr + data.RowPitch;
             srcBufMappedPtr += srcBufRowPitch;
         }
         srcBuf->GetResource()->Unmap(0, D3D12_RANGE_ALL); // pWrittenRange
-    }
-
-    // Create destination texture.
-    {
-        D3D12MA::ALLOCATION_DESC allocDesc = {};
-        allocDesc.HeapType = D3D12_HEAP_TYPE_DEFAULT;
-        CHECK_HR(g_Renderer->GetMemoryAllocator()->CreateResource(
-            &allocDesc,
-            &resDesc,
-            D3D12_RESOURCE_STATE_COPY_DEST,
-            nullptr, // pOptimizedClearValue
-            &m_Allocation,
-            IID_PPV_ARGS(&m_Resource))); // riidResource, ppvResource
-        if(!name.empty())
-            SetD3D12ObjectName(m_Resource.Get(), name);
     }
 
     // Copy the data.
@@ -138,30 +192,27 @@ void Texture::LoadFromMemory(
         CommandList cmdList;
         g_Renderer->BeginUploadCommandList(cmdList);
 
-        CD3DX12_TEXTURE_COPY_LOCATION dst{m_Resource.Get()};
+        CD3DX12_TEXTURE_COPY_LOCATION dst{m_Resource.Get(), mipLevel};
         D3D12_PLACED_SUBRESOURCE_FOOTPRINT srcFootprint = {0, // Offset
-            {resDesc.Format, (UINT)resDesc.Width, (UINT)resDesc.Height, 1, (UINT)srcBufRowPitch}};
+            {m_Desc.Format, size.x, size.y, 1, (UINT)srcBufRowPitch}};
         CD3DX12_TEXTURE_COPY_LOCATION src{srcBuf->GetResource(), srcFootprint};
         CD3DX12_BOX srcBox{
             0, 0, 0, // Left, Top, Front
-            (LONG)resDesc.Width, (LONG)resDesc.Height, 1}; // Right, Bottom, Back
+            (LONG)size.x, (LONG)size.y, 1}; // Right, Bottom, Back
 
         cmdList.GetCmdList()->CopyTextureRegion(&dst,
             0, 0, 0, // DstX, DstY, DstZ
             &src, &srcBox);
 
-        CD3DX12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::Transition(
-            m_Resource.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
-        cmdList.GetCmdList()->ResourceBarrier(1, &barrier);
+        if(lastLevel)
+        {
+            CD3DX12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::Transition(
+                m_Resource.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
+            cmdList.GetCmdList()->ResourceBarrier(1, &barrier);
+        }
 
         g_Renderer->CompleteUploadCommandList(cmdList);
     }
-
-    m_Desc = resDesc;
-
-    CreateDescriptor();
-
-    ERR_CATCH_FUNC;
 }
 
 void Texture::CreateDescriptor()
--- Source/Texture.hpp
+++ Source/Texture.hpp
@@ -9,9 +9,17 @@ Also creates and keeps its SRV descriptor.
 class Texture
 {
 public:
+    enum FLAGS
+    {
+        // Treat pixels as sRGB, create texture in sRGB format (affects LoadFromFile only).
+        FLAG_SRGB = 0x1,
+        // Generates mipmaps (affects LoadFromFile only).
+        FLAG_GENERATE_MIPMAPS = 0x2,
+    };
+
     ~Texture();
-    void LoadFromFile(const wstr_view& filePath);
-    void LoadFromMemory(
+    void LoadFromFile(uint32_t flags, const wstr_view& filePath);
+    void LoadFromMemory(uint32_t flags,
         const D3D12_RESOURCE_DESC& resDesc,
         const D3D12_SUBRESOURCE_DATA& data,
         const wstr_view& name);
@@ -30,5 +38,9 @@ private:
     D3D12_RESOURCE_DESC m_Desc = {};
     Descriptor m_Descriptor;
 
+    void CreateTexture(const wstr_view& name);
+    // lastLevel = true issues a barrier to transition texture to D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE.
+    void UploadMipLevel(uint32_t mipLevel, const uvec2& size, const D3D12_SUBRESOURCE_DATA& data,
+        bool lastLevel);
     void CreateDescriptor();
 };
